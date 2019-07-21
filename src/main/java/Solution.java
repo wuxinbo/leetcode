b9c06d845cfd4e4eb86f5bb26c664f642fb0a686
wuxinbo <wuxincaiaaa@qq.com>
2019-07-21 19:26:37 +0800

    init project

---
+++ src/main/java/Solution.java
@@ -0,0 +1,63 @@
+/**
+ * @author wuxinbo
+ * @version 1.0
+ * @since 2019/7/17
+ */
+public class Solution {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode (int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode addTwoNumbers (ListNode l1, ListNode l2) {
+        ListNode resultNode = new ListNode(0);
+        ListNode currentL1Node = l1;
+        ListNode currentL2Node = l2;
+        int carry =0;
+        //计算出来的结果
+        ListNode current = resultNode;
+        while (currentL1Node != null&&currentL2Node!=null) {
+            currentL1Node =currentL1Node==null?new ListNode(0):currentL1Node;
+            currentL2Node =currentL2Node==null?new ListNode(0):currentL2Node;
+            if (current.next==null){
+                current.next =new ListNode(0);
+            }
+            int result = current.next.val+currentL1Node.val + currentL2Node.val;
+            carry =result/10;
+            current.next = new ListNode(result%10);
+            if (carry==1){
+                current.next.val =carry;
+            }else{
+
+            }
+            current =current.next;
+            currentL1Node =currentL1Node.next;
+            currentL2Node =currentL2Node.next;
+        }
+//        if (carry==1){
+//            current.next =new ListNode(1);
+//        }
+        return resultNode.next;
+    }
+
+//    public ListNode getCurrent(){
+//
+//    }
+    public static void main (String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(9);
+//        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(9);
+//        l2.next.next = new ListNode(4);
+        ListNode listNode = addTwoNumbers(l1, l2);
+        System.out.println(listNode);
+    }
+}
+
+
---
+++ src/test/java/StringTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @author wuxinbo
+ * @version 1.0
+ * @since 2019/7/21
+ */
+public class StringTest {
+
+
+    @Test
+    public void divide(){
+        System.out.println(9/10);
+    }
+}
